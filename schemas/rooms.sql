e566d86d50d6812fb9f537ebe39a4115e316de47
Igor Padoim <igorpadoim@gmail.com>
2022-04-23 09:03:19 -0300

    Create items and checks tables

--- schemas/rooms.sql
+++ schemas/rooms.sql
@@ -16,7 +16,26 @@ CREATE TABLE IF NOT EXISTS `rooms` (
 
 CREATE TABLE IF NOT EXISTS `rooms_items` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `room` INT UNSIGNED NOT NULL, # Unfinished
+  `room` INT UNSIGNED NOT NULL,
+  `is_hidden` BOOLEAN NOT NULL DEFAULT FALSE,
+  `desc_shown` TEXT DEFAULT NULL,
+  `desc_hidden` TEXT DEFAULT NULL,
+  `investigation` TINYINT DEFAULT NULL,
+  `perception` TINYINT DEFAULT NULL,
+  `is_taken` BOOLEAN NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (`id`),
+  KEY `room` (`room`)
+);
+
+CREATE TABLE IF NOT EXISTS `rooms_checks` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `room` INT UNSIGNED NOT NULL,
+  `skill` VARCHAR(31) NOT NULL,
+  `dc` TINYINT NOT NULL DEFAULT 15,
+  `critical_failure` TEXT, # Result if roll was 5+ below DC.
+  `normal_failure` TEXT, # Result if roll was below DC.
+  `normal_success` TEXT, # Result if roll was equal-to-or-above DC.
+  `critical_success` TEXT, # Result if roll was 5+ above DC.
   PRIMARY KEY (`id`),
   KEY `room` (`room`)
 );
