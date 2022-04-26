fd57617aa469d5adb8c7c002cb7468999cc8e181
Igor Padoim <igorpadoim@gmail.com>
2022-04-26 10:50:22 -0300

    Fix `INT` fields without `UNSIGNED` keyword

--- schemas/rooms.sql
+++ schemas/rooms.sql
@@ -20,8 +20,8 @@ CREATE TABLE IF NOT EXISTS `rooms_items` (
   `is_hidden` BOOLEAN NOT NULL DEFAULT FALSE,
   `desc_shown` TINYTEXT DEFAULT NULL,
   `desc_hidden` TINYTEXT DEFAULT NULL,
-  `investigation` TINYINT DEFAULT NULL,
-  `perception` TINYINT DEFAULT NULL,
+  `investigation` TINYINT UNSIGNED DEFAULT NULL,
+  `perception` TINYINT UNSIGNED DEFAULT NULL,
   `is_taken` BOOLEAN NOT NULL DEFAULT FALSE,
   PRIMARY KEY (`id`),
   KEY `room` (`room`)
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS `rooms_checks` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `room` INT UNSIGNED NOT NULL,
   `skill` VARCHAR(31) NOT NULL,
-  `dc` TINYINT NOT NULL DEFAULT 15,
+  `dc` TINYINT UNSIGNED NOT NULL DEFAULT 15,
   `critical_failure` TEXT, # Result if roll was 5+ below DC.
   `normal_failure` TEXT, # Result if roll was below DC.
   `normal_success` TEXT, # Result if roll was equal-to-or-above DC.
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS `rooms_combats` (
   `enemies` JSON NOT NULL,
   `is_runnable` BOOLEAN NOT NULL DEFAULT FALSE,
   `is_hideable` BOOLEAN NOT NULL DEFAULT FALSE,
-  `stealth_dc` TINYINT DEFAULT NULL,
+  `stealth_dc` TINYINT UNSIGNED DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `room` (`room`)
 );
@@ -58,13 +58,13 @@ CREATE TABLE IF NOT EXISTS `rooms_exits` (
   `desc` TINYTEXT NOT NULL,
   `exit` INT UNSIGNED NOT NULL,
   `is_hidden` BOOLEAN NOT NULL DEFAULT FALSE,
-  `investigation_dc` TINYINT DEFAULT NULL,
+  `investigation_dc` TINYINT UNSIGNED DEFAULT NULL,
   `is_locked` BOOLEAN NOT NULL DEFAULT FALSE,
-  `thievestools_dc` TINYINT DEFAULT NULL,
+  `thievestools_dc` TINYINT UNSIGNED DEFAULT NULL,
   `is_breakable` BOOLEAN NOT NULL DEFAULT FALSE,
-  `strength_dc` TINYINT DEFAULT NULL,
+  `strength_dc` TINYINT UNSIGNED DEFAULT NULL,
   `has_sounds` BOOLEAN NOT NULL DEFAULT FALSE,
-  `perception_dc` TINYINT DEFAULT NULL,
+  `perception_dc` TINYINT UNSIGNED DEFAULT NULL,
   `sounds_desc` TEXT DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `room` (`room`)
