f8c704041fabcc8203673d84d4804981fa44fe21
Igor Padoim <igorpadoim@gmail.com>
2022-04-26 20:13:04 -0300

    Fix table schematics

--- install-do.php
+++ install-do.php
@@ -193,7 +193,7 @@ $tables = [
       `last_name` VARCHAR(255) DEFAULT NULL,
       `location` VARCHAR(255) DEFAULT NULL,
       `birth` DATE DEFAULT NULL,
-      `about` TEXT DEFAULT NULL,
+      `about` TINYTEXT DEFAULT NULL,
       PRIMARY KEY (`id`)
     )"
   ],
@@ -204,7 +204,7 @@ $tables = [
       `author` INT UNSIGNED NOT NULL,
       `name` VARCHAR(127) NOT NULL,
       `cover` VARCHAR(255) DEFAULT NULL,
-      `desc` MEDIUMTEXT NOT NULL,
+      `desc` TEXT NOT NULL,
       `setting` VARCHAR(63) DEFAULT NULL,
       `triggers` VARCHAR(1023) DEFAULT NULL,
       `entry` INT UNSIGNED NOT NULL,
@@ -240,14 +240,124 @@ $tables = [
       `start` DATE NOT NULL,
       `active` BOOLEAN NOT NULL DEFAULT TRUE,
       `darkvision` BOOLEAN NOT NULL DEFAULT FALSE,
-      `level` TINYINT NOT NULL,
-      `xp` MEDIUMINT DEFAULT NULL,
+      `level` TINYINT UNSIGNED NOT NULL,
+      `xp` MEDIUMINT UNSIGNED DEFAULT NULL,
       `last_saved` DATE NOT NULL,
       `last_room` INT UNSIGNED NOT NULL,
       PRIMARY KEY (`id`),
       KEY `adventure` (`adventure`),
       KEY `player` (`player`)
     )"
+  ],
+  [
+    "rooms",
+    "CREATE TABLE IF NOT EXISTS `rooms` (
+      `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+      `adventure` INT UNSIGNED NOT NULL,
+      `name` VARCHAR(63) DEFAULT NULL,
+      `desc` TEXT DEFAULT NULL,
+      `has_items` BOOLEAN DEFAULT FALSE,
+      `has_checks` BOOLEAN DEFAULT FALSE,
+      `has_combats` BOOLEAN DEFAULT FALSE,
+      `has_exits` BOOLEAN DEFAULT FALSE,
+      PRIMARY KEY (`id`),
+      KEY `adventure` (`adventure`),
+      KEY `name` (`name`)
+    )"
+  ],
+  [
+    "rooms_items",
+    "CREATE TABLE IF NOT EXISTS `rooms_items` (
+      `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+      `room` INT UNSIGNED NOT NULL,
+      `is_hidden` BOOLEAN NOT NULL DEFAULT FALSE,
+      `desc_shown` TINYTEXT DEFAULT NULL,
+      `desc_hidden` TINYTEXT DEFAULT NULL,
+      `investigation` TINYINT UNSIGNED DEFAULT NULL,
+      `perception` TINYINT UNSIGNED DEFAULT NULL,
+      `is_taken` BOOLEAN NOT NULL DEFAULT FALSE,
+      PRIMARY KEY (`id`),
+      KEY `room` (`room`)
+    )"
+  ],
+  [
+    "rooms_checks",
+    "CREATE TABLE IF NOT EXISTS `rooms_checks` (
+      `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+      `room` INT UNSIGNED NOT NULL,
+      `skill` VARCHAR(31) NOT NULL,
+      `dc` TINYINT UNSIGNED NOT NULL DEFAULT 15,
+      `critical_failure` TEXT, # Result if roll was 5+ below DC.
+      `normal_failure` TEXT, # Result if roll was below DC.
+      `normal_success` TEXT, # Result if roll was equal-to-or-above DC.
+      `critical_success` TEXT, # Result if roll was 5+ above DC.
+      PRIMARY KEY (`id`),
+      KEY `room` (`room`)
+    )"
+  ],
+  [
+    "rooms_combats",
+    "CREATE TABLE IF NOT EXISTS `rooms_combats` (
+      `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+      `room` INT UNSIGNED NOT NULL,
+      `desc` TINYTEXT NOT NULL,
+      `enemies` JSON NOT NULL,
+      `is_runnable` BOOLEAN NOT NULL DEFAULT FALSE,
+      `is_hideable` BOOLEAN NOT NULL DEFAULT FALSE,
+      `stealth_dc` TINYINT UNSIGNED DEFAULT NULL,
+      PRIMARY KEY (`id`),
+      KEY `room` (`room`)
+    )"
+  ],
+  [
+    "rooms_exits",
+    "CREATE TABLE IF NOT EXISTS `rooms_exits` (
+      `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+      `room` INT UNSIGNED NOT NULL,
+      `desc` TINYTEXT NOT NULL,
+      `exit` INT UNSIGNED NOT NULL,
+      `is_hidden` BOOLEAN NOT NULL DEFAULT FALSE,
+      `investigation_dc` TINYINT UNSIGNED DEFAULT NULL,
+      `is_locked` BOOLEAN NOT NULL DEFAULT FALSE,
+      `thievestools_dc` TINYINT UNSIGNED DEFAULT NULL,
+      `is_breakable` BOOLEAN NOT NULL DEFAULT FALSE,
+      `strength_dc` TINYINT UNSIGNED DEFAULT NULL,
+      `has_sounds` BOOLEAN NOT NULL DEFAULT FALSE,
+      `perception_dc` TINYINT UNSIGNED DEFAULT NULL,
+      `sounds_desc` TEXT DEFAULT NULL,
+      PRIMARY KEY (`id`),
+      KEY `room` (`room`)
+    )"
+  ],
+  [
+    "meta_checks",
+    "CREATE TABLE IF NOT EXISTS `meta_checks` (
+      `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+      `name` VARCHAR(63) NOT NULL,
+      PRIMARY KEY (`id`),
+      UNIQUE KEY `name` (`name`)
+    )"
+  ],
+  [
+    "meta_items",
+    "CREATE TABLE IF NOT EXISTS `meta_items` (
+      `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+      `code` VARCHAR(63),
+      `name` VARCHAR(63) NOT NULL,
+      `desc` TEXT NOT NULL,
+      `source` VARCHAR(255),
+      `page` TINYINT UNSIGNED,
+      PRIMARY KEY (`id`),
+      UNIQUE KEY `code` (`code`)
+    )"
+  ],
+  [
+    "meta_items TRIGGER",
+    "CREATE
+      DEFINER='shadows'@'localhost'
+      TRIGGER `set_item_code`
+      BEFORE INSERT ON `meta_items` FOR EACH ROW
+      SET NEW.`code` = CONCAT('IID', UNIX_TIMESTAMP())"
   ]
 ];
 
